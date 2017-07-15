a5013a281bc2af7a52216ef6012f99144539dd9f
= <=>
2017-07-15 09:48:27 +0430

    register user

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreUserRequest;
+use App\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
     public function register(StoreUserRequest $request)
     {
+        // create new user
+        $user = User::create($request->all());
 
+        // TODO send welcome email
+
+        // return user's data
     }
 }
