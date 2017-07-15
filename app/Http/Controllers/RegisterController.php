d520ae46970aaa06d72576389c3d55608e7d50bd
= <=>
2017-07-15 09:39:47 +0430

    add register controller and its request form

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreUserRequest;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function register(StoreUserRequest $request)
+    {
+
+    }
+}
